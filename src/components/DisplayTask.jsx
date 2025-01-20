e7ca44b89dbecd3b4d7baea448fe46f257a8fbe6
Pra-Tar <pranchal0112@gmail.com>
2025-01-20 04:59:27 +0000

    added updating task status functionality

--- src/components/DisplayTask.jsx
+++ src/components/DisplayTask.jsx
@@ -6,31 +6,35 @@ const DisplayTask = () => {
   const allTasks = useSelector((state) => state.tasks);
   const searchQuery = useSelector((state) => state.searchQuery);
   const filterData = useSelector((state) => state.filter);
-  const sortData = useSelector((state) => state.sort )
-  const priorityOrder = {low: 1, medium: 2, high: 3}
+  const sortData = useSelector((state) => state.sort);
+  const priorityOrder = { low: 1, medium: 2, high: 3 };
 
+  console.log(allTasks);
 
   const applyFilters = (tasks) => {
-    return tasks.filter((t) => {
-      if (filterData["pending"] && filterData["completed"]) return true;
-      if (filterData["pending"]) return t.status === "pending";
-      if (filterData["completed"]) return t.status === "completed";
-      return true;
-    }).sort((a,b)=>{
+    return tasks
+      .filter((t) => {
+        if (filterData["pending"] && filterData["completed"]) return true;
+        if (filterData["pending"]) return t.status === "pending";
+        if (filterData["completed"]) return t.status === "completed";
+        return true;
+      })
+      .sort((a, b) => {
         return sortData.date === "asc"
-        ? new Date(a.date) - new Date(b.date)
-        : new Date(b.date) - new Date(a.date) 
-    }).sort((a,b)=>{
+          ? new Date(a.date) - new Date(b.date)
+          : new Date(b.date) - new Date(a.date);
+      })
+      .sort((a, b) => {
         return sortData.priority === "asc"
-        ? priorityOrder[a.priority] - priorityOrder[b.priority]
-        : priorityOrder[b.priority] - priorityOrder[a.priority]
-    })
+          ? priorityOrder[a.priority] - priorityOrder[b.priority]
+          : priorityOrder[b.priority] - priorityOrder[a.priority];
+      });
   };
 
   const filteredTasks =
-    (searchQuery.length === 0
+    searchQuery.length === 0
       ? applyFilters(allTasks)
-      : applyFilters(searchResults));
+      : applyFilters(searchResults);
 
   if (filteredTasks.length === 0) {
     return <h1>No Results Found!</h1>;
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,38 +1,66 @@
-import { useDispatch, useSelector } from "react-redux"
-import { removeTask } from "../slices/taskSlice"
-import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { removeTask, editTask } from "../slices/taskSlice";
+import { useState } from "react";
 
-const TaskCard = ({task})=>{
-    
-    const dispatch = useDispatch()
-    const [isChecked, setisChecked] = useState((task.status==='pending'?false:true))
+const TaskCard = ({ task }) => {
+  const dispatch = useDispatch();
+  const [isChecked, setisChecked] = useState(
+    task.status === "pending" ? false : true
+  );
 
-    const handleDelete = ()=>{
-        dispatch(removeTask(task.id))
-    }
+  const handleDelete = () => {
+    dispatch(removeTask(task.id));
+  };
 
-    console.log(isChecked)
-    const handleStatus = () => {
+  const handleStatus = () => {
+    isChecked ? setisChecked(false) : setisChecked(true);
+    dispatch(
+      editTask({
+        id: task.id,
+        updates: {
+          ...task,
+          status: isChecked ? "pending" : "completed",
+        },
+      })
+    );
+  };
 
-    };
-      
-
-    return(
-        <>
-            <div className="h-52 rounded-2xl border-2 p-3 border-black" style={{ backgroundColor: task.bgColor }} >
-                <span className="block text-2xl mb-1"> {task.title.charAt(0).toUpperCase() + task.title.slice(1)} </span>
-                <div className="h-32 overflow-clip">
-                    <span className="block text-lg font-light break-words text-ellipsis">{task.description}</span>                
-                </div>
-                <div className="mt-auto flex justify-between items-center">
-                    <input type="checkbox" className="w-4 h-4" onChange={handleStatus}/>
-                    <span className="font-light text-xs">{task.priority.toUpperCase()}</span>
-                    <span className="font-light text-xs">{task.date}</span>                
-                    <button className="bg-red-700 w-4 h-4 border-black border-2 flex items-center justify-center text-sm" onClick={handleDelete}>X</button>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        className="h-52 rounded-2xl border-2 p-3 border-black"
+        style={{ backgroundColor: task.bgColor }}
+      >
+        <span className="block text-2xl mb-1">
+          {" "}
+          {task.title.charAt(0).toUpperCase() + task.title.slice(1)}{" "}
+        </span>
+        <div className="h-32 overflow-clip">
+          <span className="block text-lg font-light break-words text-ellipsis">
+            {task.description}
+          </span>
+        </div>
+        <div className="mt-auto flex justify-between items-center">
+          <input
+            type="checkbox"
+            className="w-4 h-4"
+            checked={isChecked}
+            onChange={handleStatus}
+          />
+          <span className="font-light text-xs">
+            {task.priority.toUpperCase()}
+          </span>
+          <span className="font-light text-xs">{task.date}</span>
+          <button
+            className="bg-red-700 w-4 h-4 border-black border-2 flex items-center justify-center text-sm"
+            onClick={handleDelete}
+          >
+            X
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default TaskCard;
